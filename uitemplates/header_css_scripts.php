a0da8c205151d1376954221064d5061779c4bd59
Asanetic <39214296+Asanetic@users.noreply.github.com>
2020-04-22 10:34:50 +0300

    Update header_css_scripts.php

--- uitemplates/header_css_scripts.php
+++ uitemplates/header_css_scripts.php
@@ -1,1 +1,47 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<style>
+    
+ body
+{
+    background-image: url(https://png.pngtree.com/thumb_back/fw800/back_pic/03/88/05/6257d3f69589b07.jpg);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+}
+
+.skin_plasma
+{
+height: 100vh;
+background-color: rgba(255,255,255,0.7)
+}
+
+.padding_row
+{
+margin-top: 100px!important;
+}
+
+.padding_row_gen
+{
+margin-top: 100px!important;
+}
+
+@media screen and (max-width: 700px)
+{
+.padding_row
+{
+margin-top: 100px!important;
+}
+
+.padding_row_gen
+{
+margin-top: 50px!important;
+}
+
+.skin_plasma
+{
+height: auto;
+}
+
+}
+    
+</style>
