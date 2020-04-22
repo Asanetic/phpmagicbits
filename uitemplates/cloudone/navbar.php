64e2091da44d8575091995558320363ec1c20d2c
Asanetic <39214296+Asanetic@users.noreply.github.com>
2020-04-22 08:51:44 +0300

    Create navbar.php

---
+++ uitemplates/cloudone/navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light shadow-sm fixed-top bg-white border-bottom border-primary">
+    <a class="navbar-brand text-primary" href="#" style="font-size:27px;" >
+        <img src="{app_logo}" style="width: 50px; height: 50px;" /> {appname}</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+        {app_navlinks}
+        </ul>
+        <div class="mr-2"> 
+          <img src="./img/noavatar.jpeg" class="border" style="width: 40px; height: 40px; border-radius: 50%;" />
+          Welcome John doe | <a href="#" class="link">Logout</a>
+        </div>
+    </div>
+</nav>
