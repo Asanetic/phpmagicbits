3af1b93c4ed8ac98018d0ebad6e12095eb67ed46
Asanetic <39214296+Asanetic@users.noreply.github.com>
2020-04-24 11:43:51 +0300

    Update appframe.php

--- uitemplates/appframe.php
+++ uitemplates/appframe.php
@@ -16,7 +16,7 @@ ob_start();
 </head>
 
 <body style="background-image: url('{background_image}');">
-<?php include("{navbar_path}");?>
+{navbar_path}
     <main role="main" class="container-fluid skin_plasma padding_row">
 
       <div class="row mt-5 pt-5 justify-content-center">
