582e1be8d73dbd7afdf47a2e2ff935ee06a16292
Asanetic <39214296+Asanetic@users.noreply.github.com>
2020-09-21 15:43:05 +0300

    Update phpmagicui.php

--- uitemplates/terminal/phpmagicui.php
+++ uitemplates/terminal/phpmagicui.php
@@ -741,6 +741,61 @@ if($login_resetrequest_updatepass_newacc=='updatepass'){
 
 }
 
+function create_slideshow($file_path)
+{
+
+	$carousel_str= '
+<!--------------- Start carousel ---------->
+<div id="carouselExampleCaptions" class="carousel slide w-100" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./img/ss1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./img/ss2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./img/ss3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!--------------- End carousel ---------->
+<!--<{ncgh}/>-->';
+
+	if($file_path!='')
+	{
+
+		replace_file_section($file_path, '<!--<{ncgh}/>-->', $carousel_str);
+
+	}
+}
+
+
 function  install_pdf()
 {
 	ui_write_to_file('../tcpdf.zip', file_get_contents('https://github.com/Asanetic/phpmagicbits/raw/master/uitemplates/tcpdf.zip'));
@@ -1012,6 +1067,9 @@ function fend_help()
 
 	clone_module($original_module, $new_name, $create_list, $create_profile, $create_cruds);
 
+	//============= Create  carousel
+
+	 create_slideshow($file_path)
 	';
 
 
