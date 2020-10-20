85ca29b3eabeda3ef1d858fe432acab9358b962b
Asanetic <39214296+Asanetic@users.noreply.github.com>
2020-10-20 14:08:47 +0300

    Add files via upload

---
+++ workprogress.php
@@ -0,0 +1,6 @@
+<style>
+  body{
+      margin:0;
+  }  
+</style>
+<iframe src="https://docs.google.com/spreadsheets/d/1sQA-P2G1z05rg0vrnd8q8tt_8KFVUL0klZatNYZDOz0/edit?usp=sharing" style="width: 100%; min-height: 100vh; "></iframe>
