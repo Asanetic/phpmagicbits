0827709f4ee3284d451f45080a3c916e8af9be50
Asanetic <39214296+Asanetic@users.noreply.github.com>
2021-01-21 23:37:25 +0300

    Delete uitemplates/terminal directory

--- uitemplates/terminal/terminal_exe.php
+++
@@ -1,9 +0,0 @@
-<?php //===create app frame
-
-$page_title='Contact us';
-$newfile_name='../contact.php';
-
-create_appframe($page_title, $navbar_path, $footer_path, $header_css_scripts, $newfile_name, $background_image_path, $template_path);
-
-
-?>
