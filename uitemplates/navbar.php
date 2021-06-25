5488a34fd6341a971079b43acb390f3ed0741db0
Asanetic <39214296+Asanetic@users.noreply.github.com>
2021-06-25 10:29:20 +0300

    Update navbar.php

--- uitemplates/navbar.php
+++ uitemplates/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-light  shadow-sm fixed-top  border-bottom border-light" style="background-color:rgb(250 240 247);">
+<nav class="navbar navbar-expand-md navbar-light  shadow-sm fixed-top  border-bottom border-light" style="background-color:rgb(234 246 254);">
     <a class="navbar-brand text-primary" href="#">
         <img src="{app_logo}" style="width: 50px; height: 50px;" /> {appname}</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
