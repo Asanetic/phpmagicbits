706b2f48ed90ccebb22e0713f26ff1dd5e17cb02
Asanetic <39214296+Asanetic@users.noreply.github.com>
2021-06-25 09:27:03 +0300

    Update header_css_scripts.php

--- uitemplates/header_css_scripts.php
+++ uitemplates/header_css_scripts.php
@@ -1,34 +1,57 @@
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/designer.css">
+    <link rel="stylesheet" href="./css/fonts.css">
     <link rel="icon" href="./img/logo.png">
 <style>
+  .btn_neo{
+    border-radius: 30px;
+    box-shadow:  20px 20px 60px #cfc4c4,
+             -20px -20px 60px #ffffff;
+    border:0px;
+
+}
+  .btn_neoo2{
+    border-radius: 30px;
+    background: linear-gradient(225deg, #f31277, #650129);
+    box-shadow:  -20px 20px 70px #fff,
+                20px -20px 60px #ffffff;
+    }
     
  body
 {
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
-}
-.form-control label
-{
-    font-weight:bold;
-}
-.cpointer
-{
-cursor:pointer;
+    background-image: url('./img/bg.jpg');
+    background: linear-gradient(0deg, rgba(168,190,214,0.21612394957983194) 0%, rgba(180,8,126,1) 100%);
 }
     
+.bg_grad
+{
+  background: rgb(238,174,202);
+  background: radial-gradient(circle, rgba(238,174,202,0.2553396358543417) 12%, rgba(156,193,236,0.24693627450980393) 49%);
+}
+    
+.form-control{
+  background-color:transparent;
+  border:none;
+  border-bottom:1px solid green;
+  border-radius:0px;
+}
+    
+.form-group label
+{
+font-weight:bold;  
+}
+
+.cpointer
+{
+    cursor: pointer;
+}
+
 .skin_plasma
 {
 height: auto;
 background-color: rgba(255,255,255,0.7)
-}
-.form-group label{
- font-weight:bold;
- }
-    
-.padding_row
-{
-margin-top: 70px!important;
 }
 
 .padding_row_gen
@@ -42,6 +65,14 @@ margin-top: 65px!important;
 .navbar-brand{
     font-size:27px;
 }
+.btn-primary{
+	margin-bottom: 14px;
+    border-radius: 30px;
+    background: linear-gradient(225deg, purple, pink);
+    box-shadow:  -20px 20px 70px lightblue,
+             20px -20px 60px #ffffff;
+}
+
 @media screen and (max-width: 700px)
 {
 .padding_row
