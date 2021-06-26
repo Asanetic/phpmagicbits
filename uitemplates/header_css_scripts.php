3bba4b8bdb139b0c8f5775e466f942d37e226448
Asanetic <39214296+Asanetic@users.noreply.github.com>
2021-06-26 12:28:34 +0300

    Update header_css_scripts.php

--- uitemplates/header_css_scripts.php
+++ uitemplates/header_css_scripts.php
@@ -1,50 +1,54 @@
     <link rel="stylesheet" href="./css/designer.css">
     <link rel="stylesheet" href="./css/fonts.css">
-    <link rel="icon" href="./img/logo.png">
+    <link rel="icon" href="./img/logo_gr.png">
 <style>
-  .btn_neo{
-    border-radius: 30px;
-    box-shadow:  20px 20px 60px #cfc4c4,
-             -20px -20px 60px #ffffff;
-    border:0px;
+.btn_neo{
+  border-radius: 30px;
+  box-shadow:  20px 20px 60px #dc3545,
+               -20px -20px 60px #ffffff;
+  border:0px;
 
 }
-  .btn_neoo2{
-    border-radius: 30px;
-    background: linear-gradient(225deg, #f31277, #650129);
-    box-shadow:  -20px 20px 70px #fff,
-                20px -20px 60px #ffffff;
-    }
-    
+
+.btn_neoo2
+{
+  border-radius: 30px;
+  background: linear-gradient(225deg, #B50203, #000);
+  box-shadow:  -10px 10px 90px #dc3545,
+               10px -10px 50px #ffffff;
+}  
+                            
+.bg-primary{
+  background-color:#B40202!important;
+} 
+            
  body
 {
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
     background-image: url('./img/bg.jpg');
-    background: linear-gradient(0deg, rgba(168,190,214,0.21612394957983194) 0%, rgba(180,8,126,1) 100%);
+	background: linear-gradient(0deg, rgba(168,190,214,0.21612394957983194) 0%, rgba(192,0,0,1) 100%);
 }
-    
-.bg_grad
-{
+                              
+.bg_grad{
   background: rgb(238,174,202);
-  background: radial-gradient(circle, rgba(238,174,202,0.2553396358543417) 12%, rgba(156,193,236,0.24693627450980393) 49%);
+background: radial-gradient(circle, rgba(238,174,202,0.2553396358543417) 12%, rgba(0,148,240,0.24693627450980393) 49%);
 }
-    
+                              
 .form-control{
   background-color:transparent;
   border:none;
   border-bottom:1px solid green;
   border-radius:0px;
 }
-    
+                              
 .form-group label
 {
 font-weight:bold;  
 }
 
-.cpointer
-{
+.cpointer{
     cursor: pointer;
 }
 
@@ -58,26 +62,29 @@ background-color: rgba(255,255,255,0.7)
 {
 margin-top: 100px!important;
 }
+                              
 .padding_row
 {
 margin-top: 65px!important;
 }
+                              
 .navbar-brand{
     font-size:27px;
 }
+                              
 .btn-primary{
-	margin-bottom: 14px;
-    border-radius: 30px;
-    background: linear-gradient(225deg, purple, pink);
-    box-shadow:  -20px 20px 70px lightblue,
-             20px -20px 60px #ffffff;
+  border-radius: 30px;
+  background: linear-gradient(225deg, #B50203, #000);
+  box-shadow:  -10px 10px 90px #ccc,
+               10px -10px 50px #ffffff;
+  border:0px;                            
 }
 
 @media screen and (max-width: 700px)
 {
 .padding_row
 {
-margin-top: 65px!important;
+	margin-top: 65px!important;
 }
     
 .navbar-brand{
@@ -86,12 +93,12 @@ margin-top: 65px!important;
 
 .padding_row_gen
 {
-margin-top: 50px!important;
+	margin-top: 50px!important;
 }
 
 .skin_plasma
 {
-height: auto;
+	height: auto;
 }
 
 }
