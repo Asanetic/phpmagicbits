6c31f3b2cc9957f0100ead8f8e6925aa716b8a1d
Asanetic <39214296+Asanetic@users.noreply.github.com>
2021-06-28 12:40:55 +0300

    Update navbar.php

--- uitemplates/navbar.php
+++ uitemplates/navbar.php
@@ -7,13 +7,13 @@
 
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
+            <!--<li class="nav-item">
                 <a class="nav-link" href="#">{app_navlinks}</a>
-            </li>
-       
+            </li>-->
+        <!--nav_nodes-->
         </ul>
         <div class="mr-2"> 
-          <img src="https://raw.githubusercontent.com/Asanetic/phpmagicbits/master/uitemplates/noavatar.jpeg" class="border" style="width: 40px; height: 40px; border-radius: 50%;" />
+          <!--<img src="{app_logo}" class="border" style="width: 40px; height: 40px; border-radius: 50%;" />-->
           <!--login_str-->
         </div>
     </div>
