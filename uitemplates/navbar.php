77378cc76a4c9e81a967f4f197e92ccf887140fe
Asanetic <39214296+Asanetic@users.noreply.github.com>
2021-11-04 07:58:19 +0300

    Update navbar.php

--- uitemplates/navbar.php
+++ uitemplates/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light p-0 <?php echo $nav_shadow_class; ?> fixed-top nav_bar_set">
     <a class="navbar-brand text-dark" href="#">
         <img src="<?php echo $mep_app_logo ?>" class="mr-2" style="<?php  echo $mep_app_logo_style; ?>" /> <?php echo $mep_app_name; ?></a>
-    <button class="navbar-toggler pr-3 border-0" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler pr-2 border-0" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
